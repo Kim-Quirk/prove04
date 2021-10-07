a950c77af453cd4c81227d113167c48e3f061b2d
Kim Black <kimblackaz@gmail.com>
2021-10-07 10:29:53 -0600

    Lesson 04

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const path = require('path');
-const PORT = process.env.PORT || 3000;
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors')
+
+const PORT = process.env.PORT || 3000
 
 const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
@@ -14,12 +18,59 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const corsOptions = {
+  origin: "https://cse341-ecommerce-app.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://user:pass@cluster0.daotz.mongodb.net/shop?retryWrites=true&w=majority";
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('615f1ac8ee90a9679d7247dc')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(PORT);
+mongoose
+  .connect(
+    'mongodb+srv://user:pass@cluster0.daotz.mongodb.net/shop?retryWrites=true&w=majority', options
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
